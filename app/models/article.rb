da1435955b121e57c6dd8d50ef7b8866c817bf0e
Jackie Pan <jacpan1120@gmail.com>
2015-04-20 20:28:07 -0400

    hello

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
 	has_many :categories
 
-	validate_presence_of :title, :content
+	validates_presence_of :title, :content
 
 	scope :alphabetical, order('name')
 	scope :active, where('active = ?', true)
